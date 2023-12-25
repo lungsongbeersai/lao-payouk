816f2f9c4c2733772bc871e7773bfe1cba9ae865
lungsongbeersai <lungsongbeersai@gmail.com>
2023-12-25 16:04:22 +0700

    api

--- assets/js/socket_connect.js
+++ assets/js/socket_connect.js
@@ -1,3 +1,3 @@
 urlApi='https://pos.restaurant.laopayook.com/api/';
-urlImage='https://pos.restaurant.laopayook.comapi/images/';
+urlImage='https://pos.restaurant.laopayook.com/api/images/';
 const socket = io.connect('https://pos.restaurant.laopayook.com:5000');
